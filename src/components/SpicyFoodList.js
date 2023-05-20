5c84d32a0fd8194555c5a2e2b12057e257d8aad7
DennisMatenge <mateshdennis@gmail.com>
2023-05-20 19:07:15 +0300

    first commit

---
+++ src/components/SpicyFoodList.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { spicyFoods, getNewRandomSpicyFood } from "../data";
+
+function SpicyFoodList() {
+  const [foods, setFoods] = useState(spicyFoods);
+
+  function handleAddFood() {
+    const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray);
+  }
+
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick = {() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  ));
+  
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  return (
+    <div>
+      <button onClick={handleAddFood}>Add New Food</button>
+      <ul>{foodList}</ul>
+    </div>
+  );
+}
+
+export default SpicyFoodList;
